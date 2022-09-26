0eabc3dce2d999103974278bb3b8e609ab68c230
root <naomieloor@gmail.com>
2022-09-26 08:49:44 -0700

    Task 4

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,2 @@
- (2 sloc)  81 Bytes
 #!/usr/bin/python3
 print('\"Programming is like building a multilingual puzzle')
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+ (3 sloc)  66 Bytes
+#!/usr/bin/python3
+number = 3.14159
+print(f"Float: {number:.2f}")
